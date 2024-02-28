72c372385825bc9f330ea3c3580c769f5de047a1
Blankanator <7fuzi.blanka@gmail.com>
2024-02-28 16:56:06 +0100

    2 feladat

--- gyakorlo.sql
+++ gyakorlo.sql
@@ -1,3 +1,15 @@
-SELECT *
+-- 1 feladat
+SELECT *, 
+    IIF(MONTH(METTOL) IN (6, 7, 8), 'Igen', 'Nem') AS 'Nyári szezon'
 FROM Foglalas
 WHERE UGYFEL_FK = 'laszlo2' AND GYERMEK_SZAM = 0
+
+-- 2 feladat
+SELECT szh.TIPUS, 
+       YEAR(f.METTOL) AS 'Év', 
+       MONTH(f.METTOL) AS 'Hónap',
+       COUNT(*) AS 'foglalások száma'
+FROM Foglalas f JOIN Szoba sz ON f.SZOBA_FK = sz.SZOBA_ID 
+                JOIN Szallashely szh ON szh.SZALLAS_ID = sz.SZALLAS_FK
+WHERE DATEDIFF(day, METTOL, MEDDIG) >=5 
+GROUP BY szh.TIPUS, YEAR(f.METTOL), MONTH(f.METTOL)
