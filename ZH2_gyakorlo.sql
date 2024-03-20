f7b4d442b88696fdf3dd6a7d2a9439dbbda7bc9b
Blankanator <7fuzi.blanka@gmail.com>
2024-03-20 16:56:46 +0100

    2. feladat

--- ZH2_gyakorlo.sql
+++ ZH2_gyakorlo.sql
@@ -1,6 +1,16 @@
+--- 1. feladat
 SELECT IIF(v.USERNEV IS NULL, 'Végösszeg', v.USERNEV), COUNT(DISTINCT szh.SZALLAS_ID) as 'Folgalások'
 FROM Vendeg v
     LEFT JOIN foglalas f ON v.USERNEV = f.ugyfel_fk
     LEFT JOIN szoba sz ON sz.SZOBA_ID = f.szoba_FK
     LEFT JOIN szallashely szh ON szh.SZALLAS_ID = sz.SZALLAS_FK
 GROUP BY ROLLUP(v.USERNEV)
+
+--- 2. feladat
+SELECT 
+    *, 
+    DATEDIFF(YEAR, SZUL_DAT, getdate()) AS 'Kor',
+    AVG(DATEDIFF(YEAR, SZUL_DAT, getdate())) OVER(
+        ORDER BY SZUL_DAT 
+        ROWS BETWEEN 2 PRECEDING AND 2 FOLLOWING) AS 'Átlag kor'
+FROM Vendeg
